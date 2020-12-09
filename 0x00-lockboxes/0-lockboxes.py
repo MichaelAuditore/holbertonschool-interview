3b8b56e6a60b6c5529f367b7093c27cfb44f4b46
MichaelAuditore <michael_v613@hotmail.com>
2020-12-09 10:35:32 -0500

    [Pep8 fixed]

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """Python script has a function canUnlockAll and checkKeys"""
 
+
 def canUnlockAll(boxes):
     """canUnlockAll - function to check if a list of lists contains all keys
     to open all boxes"""
     keys = [0]
     for key in keys:
-        for item in boxes[key]:
-            if (item not in keys):
-                keys.append(item)
+        if (boxes[key]):
+            for item in boxes[key]:
+                if (item not in keys):
+                    keys.append(item)
+
     if (len(keys) == len(boxes)):
         return True
     return False
