1bb166ffab7edffbabf77dca2c181b9564c74686
MichaelAuditore <michael_v613@hotmail.com>
2020-12-09 11:08:49 -0500

    [New conditios] Check if key is less than length of my list of boxes and if length of boxes is equal to 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,13 +6,18 @@ def canUnlockAll(boxes):
     """canUnlockAll - function to check if a list of lists contains all keys
     to open all boxes"""
     keys = [0]
+    if (len(boxes) == 0):
+        return True
+
     for key in keys:
-        if (boxes[key]):
-            for item in boxes[key]:
-                if (item not in keys):
+        box = boxes[key]
+        for item in box:
+            if (item not in keys):
+                if (item <= len(boxes) - 1):
                     keys.append(item)
-        else:
-            return False
+            else:
+                continue
+
     if (len(keys) == len(boxes)):
         return True
     return False
