3bb974360606bbeee9d6bd24b686586e7d771a29
MichaelAuditore <michael_v613@hotmail.com>
2020-12-09 11:13:51 -0500

    [Test Case]

--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -5,6 +5,10 @@ canUnlockAll = __import__('0-lockboxes').canUnlockAll
 boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes), "\t: True")
 
+boxes = [[1], [5], [3], [4], []]
+print(canUnlockAll(boxes), "\t: False")
+
+
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 print(canUnlockAll(boxes), "\t: True")
 
@@ -12,7 +16,7 @@ boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes), "\t: False")
 
 boxes = [[1], [2], [3], [4], [5], [6], [7], [0]]
-print(canUnlockAll(boxes), "\t: True\n")
+print(canUnlockAll(boxes), "\t: True")
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6], [7]]
 print(canUnlockAll(boxes), "\t: False")
