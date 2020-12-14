54b39614c4f1a43ba0dcc5ca0a95a80534432bb4
MichaelAuditore <michael_v613@hotmail.com>
2020-12-14 10:26:16 -0500

    [README.md] init directory [0-insert_number.c] Code to insert a number into a orderd singly linked list [lists.h] header file to call libraries and functions

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to pointer of first element in singly linked list
+ * @number: number to insert into linked list
+ * Return: the address of the new node or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *tmp = *head;
+
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			if (tmp->n < new->n && tmp->next->n > new->n)
+			{
+				break;
+			}
+			tmp = tmp->next;
+		}
+		new->next = tmp->next;
+		tmp->next = new;
+	}
+	return (new);
+}
