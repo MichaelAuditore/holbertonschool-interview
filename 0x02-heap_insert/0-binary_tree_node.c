06c7938e028f507ab237f737fca44968ab85bb0f
MichaelAuditore <michael_v613@hotmail.com>
2020-12-22 11:38:14 -0500

    [Fix: Just a simple node]

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -8,30 +8,17 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = NULL;
 
-	if (!new)
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
 		return (NULL);
 
-	new->n = value;
-	new->left = new->right = NULL;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
 
-	if (!parent)
-	{
-		parent = new;
-		return (parent);
-	}
-
-	if (new->n < parent->n)
-	{
-		parent->left = new;
-		new->parent = parent;
-	}
-
-	if (new->n > parent->n)
-	{
-		parent->right = new;
-		new->parent = parent;
-	}
-	return (new);
+	return (new_node);
 }
